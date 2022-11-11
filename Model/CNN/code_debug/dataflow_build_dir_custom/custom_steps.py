4a6146efb909343c5ef7dcb3b4fd96b91f2cc9cb
auphelia <jakobapk@web.de>
2022-11-11 18:10:25 +0000

    [custom_steps] Modify custom convert_to_hls layers to match network topology

--- Model/CNN/code_debug/dataflow_build_dir_custom/custom_steps.py
+++ Model/CNN/code_debug/dataflow_build_dir_custom/custom_steps.py
@@ -14,25 +14,19 @@ from qonnx.transformation.infer_shapes import InferShapes
 from qonnx.transformation.make_input_chanlast import MakeInputChannelsLast
 
 def custom_step_convert_to_hls(model, cfg):
-    model = model.transform(MakeInputChannelsLast())
-    model = model.transform(AbsorbTransposeIntoMultiThreshold())
-    model = model.transform(AbsorbConsecutiveTransposes())
     model = model.transform(InferDataTypes())
     model = model.transform(GiveUniqueNodeNames())
     model = model.transform(GiveReadableTensorNames())
     model = model.transform(to_hls.InferThresholdingLayer())
+    model = model.transform(to_hls.InferPool_Batch())
     model = model.transform(to_hls.InferConvInpGen())
     model = model.transform(to_hls.InferVectorVectorActivation())
     model = model.transform(
         to_hls.InferQuantizedMatrixVectorActivation(mem_mode="decoupled")
     )
-    print("\n","before infer bool".center(40, '#'), "\n")
-    model = model.transform(to_hls.InferPool_Batch())
-    print("\n","after infer bool".center(40, '#'), "\n")
-    print("\n","before inferstreaming bool".center(40, '#'), "\n")
-    model = model.transform(to_hls.InferStreamingMaxPool())
-    print("\n","after inferstreaming bool".center(40, '#'), "\n")
     model = model.transform(RemoveCNVtoFCFlatten())
+    model = model.transform(AbsorbTransposeIntoMultiThreshold())
+    model = model.transform(AbsorbConsecutiveTransposes())
     model = model.transform(InferDataTypes())
     model = model.transform(GiveUniqueNodeNames())
     model = model.transform(GiveReadableTensorNames())
